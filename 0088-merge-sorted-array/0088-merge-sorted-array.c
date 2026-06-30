f126263ef3f674e7a2b53b83567548d2938318c1
Romyajit-Sahoo <romyajitsahoo@gmail.com>
2026-06-30 23:22:46 +0530

    Time: 4 ms (1.24%), Space: 10.6 MB (92.76%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.c
+++ 0088-merge-sorted-array/0088-merge-sorted-array.c
@@ -7,15 +7,20 @@ void merge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size, int n)
         nums1[i] = nums2[left];
         left++;
     }
-    for (int i = 1; i < nums1Size; i++)
+    for (int i = 0; i < nums1Size - 1; i++)
     {
-        currnt = nums1[i];
-        prev = i - 1;
-        while (prev >= 0 && nums1[prev] > currnt)
+        int smallest_index = i;
+
+        for (int j = i + 1; j < nums1Size; j++)
         {
-            nums1[prev + 1] = nums1[prev];
-            prev--;
+            if (nums1[j] < nums1[smallest_index])
+            {
+                smallest_index = j;
+            }
         }
-        nums1[prev + 1] = currnt;
+        // Swap after inner loop
+        int temp = nums1[i];
+        nums1[i] = nums1[smallest_index];
+        nums1[smallest_index] = temp;
     }
 }
