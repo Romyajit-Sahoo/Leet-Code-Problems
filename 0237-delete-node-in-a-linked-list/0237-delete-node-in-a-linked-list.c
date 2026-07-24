7d37e95420700f3f910aeeb894bdbc5bb47c0e3a
Romyajit-Sahoo <romyajitsahoo@gmail.com>
2026-07-24 23:11:39 +0530

    Time: 4 ms (79.8%), Space: 11.7 MB (35.51%) - LeetHub

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.c
@@ -0,0 +1,16 @@
+// #include <stdlib.h>
+// struct ListNode
+// {
+//     int val;
+//     struct ListNode *next;
+// };
+
+void deleteNode(struct ListNode *node)
+{
+    struct ListNode *temp = node;
+
+    node->val = node->next->val;
+    node = node->next;
+    temp->next = node->next;
+    free(node);
+}
